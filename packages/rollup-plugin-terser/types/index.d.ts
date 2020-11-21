7a332bc727e0b7310305cadcec50323c1eb90c1e
Prana Adiwira <intrnl28+github@gmail.com>
2020-11-21 11:30:59 +0700

    chore: remove autogenerated types

--- packages/rollup-plugin-terser/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Plugin } from 'rollup';
-import type { MinifyOptions } from 'terser';
-export declare function terser(opts?: PluginOptions): Plugin;
-export interface PluginOptions {
-    numWorkers?: number;
-    minifyOptions?: MinifyOptions;
-}
